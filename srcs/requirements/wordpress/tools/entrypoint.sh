b596a28c9efd35f15e76f650c976258d1bbfd003
Youssef-Nini <youssefagadir2005@gmail.com>
2026-08-31 10:35:02 -0400

    finish wordpress servise

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+cd /var/www/html
+
+for i in {1..30}; do
+	if mysqladmin ping -h "mariadb" --silent; then
+		break
+	fi
+	sleep 1
+done
+
+if ! wp core is-installed --allow-root &>/dev/null; then
+	if [ ! -f "index.php" ]; then
+		wp core download --allow-root
+	fi
+
+	wp config create \ 
+		--dbname="${MYSQL_DATABASE}" \
+		--dbuser="${MYSQL_USER}" \
+		--dbpass="${MYSQL_PASSWORD}" \
+		--dbhost="mariadb:3306" \
+		--allow-root
+
+	wp core install \
+		--url="${DOMAIN_NAME}" \
+		--title="${WORDPRESS_TITLE}" \
+		--admin_user="${WP_ADMIN_PASSWORD}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		--skip-email
+		--allow-root
+
+	wp user create \
+		"${WP_USER}" \
+		"${WP_EMAIL}" \
+		--user_pass="${WP_USER_PASSWORD}" \
+		--role=author
+		--allow-rote
+fi
+
+chown -R www-data:www-data /var/www/html
+exec php-fpm8.2 -F
