cc9f704bae08ae214ceae4c0f73f38ba34f643a2
Youssef-Nini <youssefagadir2005@gmail.com>
2026-09-04 10:31:18 -0400

    f

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-
 set -e
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1
+mkdir -p /var/run/mysqld
+chown -R  mysql:mysql /var/run/mysqld
 
-	mysqld --user=mysql --datadir=/var/lib/mysql &
-	pid="$!"
-	echo "here 1"
-	for i in {1..30}; do
-		if mysqladmin ping >/dev/null 2>&1; then
-			break
-		fi
-		sleep 1
-	done
+if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+	echo "Initializing MariaDB..."
+	mariadb-install-db  --user=mysql --datadir=/var/lib/mysql > /dev/null
 
-	echo "here 2"
-	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-	mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
-	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-	mysql -u root -e "FLUSH PRIVILEGES;"
+	mariadbd --user=mysql --bootstrap << EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
+CREATE USER IF NOT EXISTS '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
 
-	kill "$pid"
-	wait "$pid"q
+	echo "MariaDB initialized!"
 fi
-	echo "here 3"
 
-exec mysqld --user=mysql --datadir=/var/lib/mysql
+exec mariadbd -u mysql
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -20,25 +20,25 @@ if ! wp core is-installed --allow-root &>/dev/null; then
 	fi
 
 	wp config create \
-		--dbname="${MYSQL_DATABASE}" \
-		--dbuser="${MYSQL_USER}" \
-		--dbpass="${MYSQL_PASSWORD}" \
+		--dbname=$MYSQL_DATABASE \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD \
 		--dbhost="mariadb:3306" \
 		--allow-root
 
 	wp core install \
-		--url="${DOMAIN_NAME}" \
-		--title="${WORDPRESS_TITLE}" \
-        --admin_user="${WP_ADMIN_USER}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-		--admin_email="${WP_ADMIN_EMAIL}" \
+		--url=$DOMAIN_NAME \
+		--title=$WORDPRESS_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL \
 		--skip-email \
 		--allow-root
 
 	wp user create \
-		"${WP_USER}" \
-		"${WP_EMAIL}" \
-		--user_pass="${WP_USER_PASSWORD}" \
+		$WP_USER \
+		$WP_USER_EMAIL \
+		--user_pass=$WP_USER_PASSWORD \
 		--role=author \
 		--allow-root
 fi
